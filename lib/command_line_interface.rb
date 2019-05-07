5d729f3b4473617ffc308126d61f376c1fb5a993
Anthony Rurup <tcrurup@gmail.com>
2019-05-07 15:42:18 +0000

    added interface to CommandLineInterface

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,12 +1,26 @@
 class CommandLineInterface
   
   
+  
+  
   def intialize
   
   end
   
   def run
     self.create_all_classes
+    self.interface
+    
+  end
+  
+  def interface 
+    puts "Welcome to the Wow Class Helper"
+    puts "What class would you like to look at?"
+    chosen_class = nil
+    while chosen_class.nil? 
+      input = gets.strip
+      PlayerClass.find_by_class_name(input).nil?
+    end
   end
   
   def create_all_classes
