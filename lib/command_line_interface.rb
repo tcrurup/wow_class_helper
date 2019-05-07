c910d8ab9853f5d7b5a4eb10229aed7683d15800
Anthony Rurup <tcrurup@gmail.com>
2019-05-07 06:40:34 +0000

    Scraper also grabs specializations for each class as a hash

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -2,5 +2,17 @@ class CommandLineInterface
   
   
   def intialize
+  
   end
+  
+  def run
+    
+    self.create_all_classes
+  end
+  
+  def create_all_classes
+    classes = Scraper.scrape_base_classes
+  end
+  
+  
 end
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -12,11 +12,25 @@ class Scraper
   def self.scrape_base_classes
     page = Nokogiri::HTML(open(BASE_PATH))  
     classes = page.css("div.nav_content_block.nav_content_block_wow_class")
+    all_classes = []
     
     classes.each do |player_class|
       class_hash = {}
       class_hash[:name] = player_class.css("div.nav_content_block_title span:last-child").text
+      
+      spec_hash = {}
+      player_class.css("div.nav_content_block_entry a").each do |specialization|
+        spec_name = specialization.text
+        unless spec_name.include?("Leveling")
+          spec_url = specialization.attribute("href").text
+          spec_hash[spec_name.to_sym] = spec_url
+        end
+      end
+      
+      class_hash[:specializations] = spec_hash
+      all_classes << class_hash
     end
     
+    all_classes
   end
 end
